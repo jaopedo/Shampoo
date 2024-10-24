bd0b65b09a725b708f8607f445d1a2f627ae6cd9
jaopedo <151550223+jaopedo@users.noreply.github.com>
2024-10-24 12:05:49 -0300

    Update shampoo.js

--- shampoo.js
+++ shampoo.js
@@ -1,1 +1,18 @@
+  
+        const images = ['Mario.jpg', 'Mario2.jpg', 'morris.webp'];
+        let currentImageIndex = 0;
 
+        function changeImage() {
+            const image = document.querySelector('img');
+            currentImageIndex = (currentImageIndex + 1) % images.length;
+            image.src = images[currentImageIndex];
+        }
+  
+  function changeImage() {
+            const image = document.querySelector('img');
+            if (image.src.includes('Mario.jpg')) {
+                image.src = 'mario-bros.jpg';
+            } else {
+                image.src = 'mario-bros.jpg';
+            }
+        }
